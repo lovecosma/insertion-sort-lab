1e11cc0e238f24927f9a506e2bff8871d8ec4cdc
Matthew Williams <love.cosma@gmail.com>
2021-07-27 15:41:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
 
+    let min = array[0]
+    let counter = 0
+    let index = 0
+     for(let element of array){
+         if(element < min){
+             min = element
+            index = counter
+         } 
+         counter ++
+     }
+     array.splice(index, 1)
+     return min
 }
 
 function selectionSort(array){
+    let sorted = []
 
+    while(array.length != 0){
+        let min = findMinAndRemove(array)
+        sorted.push(min)
+    }
+    return sorted
 }
